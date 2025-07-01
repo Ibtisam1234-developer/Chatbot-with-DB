fa3a25b7afd77482f3ca3a8208a9faf94f58b8b8
Ibtisam1234-developer <muhammadibtisamasim@gmail.com>
2025-07-01 22:25:26 +0500

    Delete init.sql

--- init.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS chat_messages;
-
-CREATE TABLE chat_messages (
-    id SERIAL PRIMARY KEY,
-    role TEXT NOT NULL,
-    content TEXT NOT NULL,
-    timestamp TIMESTAMPTZ DEFAULT NOW()
-);
